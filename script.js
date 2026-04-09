fbdef18ed0957ea4ed1ceb09a3990e561eeb9419
Vivek-kumar-prajapati <148374468+Vivek-kumar-prajapati@users.noreply.github.com>
2026-04-09 22:30:01 +0800

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 //your JS code here. If required.
-https://www.svgrepo.com/show/345221/three-dots.svg
+https://www.svgrepo.com/show/345221/three-dots.svghttps://www.svgrepo.com/show/345221/three-dots.svg
